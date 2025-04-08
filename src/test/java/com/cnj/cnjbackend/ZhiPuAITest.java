a72a721dde883e13818f237ab27a636901b319b5
chennanjun12138 <2781417315@qq.com>
2025-04-08 19:50:28 +0800

    添加AI功能，实现智能化

---
+++ src/test/java/com/cnj/cnjbackend/ZhiPuAITest.java
@@ -0,0 +1,43 @@
+package com.cnj.cnjbackend;
+
+import com.zhipu.oapi.ClientV4;
+import com.zhipu.oapi.Constants;
+import com.zhipu.oapi.service.v4.model.ChatCompletionRequest;
+import com.zhipu.oapi.service.v4.model.ChatMessage;
+import com.zhipu.oapi.service.v4.model.ChatMessageRole;
+import com.zhipu.oapi.service.v4.model.ModelApiResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class ZhiPuAITest {
+    @Resource
+    private ClientV4 clientV4;
+    @Test
+    public void test() {
+        // String apiKey = "bc1211b37915420eb0839f3804e2863a.S6G67wyE8K6o1zLD";
+        // 创建客户端
+        //ClientV4 client = new ClientV4.Builder(apiKey).build();
+        // 构造请求
+        List<ChatMessage> messages = new ArrayList<>();
+        ChatMessage chatMessage = new ChatMessage(ChatMessageRole.USER.value(), "作为一名营销专家，请为智谱开放平台创作一个吸引人的slogan");
+        messages.add(chatMessage);
+        String requestId = String.valueOf(System.currentTimeMillis());
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest.builder()
+                .model(Constants.ModelChatGLM4)
+                .stream(Boolean.FALSE)
+                .invokeMethod(Constants.invokeMethod)
+                .messages(messages)
+                .requestId(requestId)
+                .build();
+        // 调用
+        ModelApiResponse invokeModelApiResp = clientV4.invokeModelApi(chatCompletionRequest);
+        System.out.println("model output:" + invokeModelApiResp.getMsg());
+    }
+}
+
+
